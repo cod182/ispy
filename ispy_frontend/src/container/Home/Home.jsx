48642f95a9bd980098984ddcec48cdddfa35a495
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-02-22 17:35:59 +0000

    started home. created pins container, sidebar & user profile components

--- ispy_frontend/src/container/Home/Home.jsx
+++ ispy_frontend/src/container/Home/Home.jsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../../components/index';
+import { client } from '../../client';
+import logo from '../../assets/logo.png';
+import Pins from '../Pins/Pins';
 
 const home = () => {
   return <div>home</div>;
