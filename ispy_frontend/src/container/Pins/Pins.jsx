e767c1db00293d92b3ddbabea44bd9528ae4636f
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-02-25 17:34:30 +0000

    added search navigation bar

--- ispy_frontend/src/container/Pins/Pins.jsx
+++ ispy_frontend/src/container/Pins/Pins.jsx
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import {}
+import { NavBar, Feed, PinDetail, CreatePin, Search } from '../../components';
 
-const Pins = () => {
-  return <div>Pins</div>;
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-grey-50">
+        <NavBar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user} />}
+          />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search usearchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
