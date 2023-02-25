78e0522b1209d011d4fcdbce6e83fc71e38a71d4
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-02-25 18:32:03 +0000

    created query to retrieve posts from sanity

---
+++ ispy_frontend/src/components/MasonryLayout/MasonryLayout.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Masonry from 'react-masonry-css';
+
+import { Pin } from '../index';
+
+const breakpointObject = {
+  default: 4,
+  3000: 6,
+  2000: 5,
+  1200: 3,
+  1000: 2,
+  500: 1,
+};
+
+const MasonryLayout = ({ pins }) => {
+  return (
+    <div>
+      <Masonry
+        className="flex animate-slide-fwd"
+        breakpointCols={breakpointObject}
+      >
+        {pins?.map((pin) => (
+          <Pin key={pin._id} pin={pin} />
+        ))}
+      </Masonry>
+    </div>
+  );
+};
+
+export default MasonryLayout;
---
+++ ispy_frontend/src/components/Pin/Pin.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Pin = ({ pin }) => {
+  console.log(pin);
+  return <div className="text-black">Pin</div>;
+};
+
+export default Pin;
