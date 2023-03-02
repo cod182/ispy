26febb60ffde4dc950e080926388a41967837a33
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-02 17:34:37 +0000

    starting the create a pin feature

--- ispy_frontend/src/components/CreatePin/CreatePin.jsx
+++ ispy_frontend/src/components/CreatePin/CreatePin.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
 
-const CreatePin = () => {
+import { client } from '../../client';
+import { Spinner } from '../index';
+import { categories } from '../../utils/data';
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
-    <div>CreatePin</div>
-  )
-}
+    <div className="fle flex-col justify-center items-center mt-5 lg:h-4/5"></div>
+  );
+};
 
-export default CreatePin
+export default CreatePin;
