cd29a74f56d60c52380fdfe001eb91dab27ebf8c
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-03 17:52:14 +0000

    added loading and func to get pin detail

--- ispy_frontend/src/components/PinDetail/PinDetail.jsx
+++ ispy_frontend/src/components/PinDetail/PinDetail.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
-  return <div>PinDetailStarting</div>;
+import { client, urlFor } from '../../client';
+import { MasonryLayout, Spinner } from '../index';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../../utils/data';
+
+const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
+  const { pinId } = useParams();
+
+  const fetchPinDetails = () => {
+    let query = pinDetailQuery(pinId);
+    if (query) {
+      client.fetch(query).then((data) => {
+        setPinDetail[0];
+        if (data[0]) {
+          query = pinDetailMorePinQuery(data[0]);
+
+          client.fetch(query).then((res) => setPins(res));
+        }
+      });
+    }
+  };
+
+  if (!pinDetail) {
+    return <Spinner message="Loading Pin..." />;
+  }
+  return <div className=""></div>;
 };
 
 export default PinDetail;
