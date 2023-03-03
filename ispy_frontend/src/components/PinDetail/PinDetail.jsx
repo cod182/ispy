5b52d6eaf895b19351b0415ad7b15ff9cc7c511d
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-03 21:04:37 +0000

    added posting/viewing comments

--- ispy_frontend/src/components/PinDetail/PinDetail.jsx
+++ ispy_frontend/src/components/PinDetail/PinDetail.jsx
@@ -14,8 +14,32 @@ const PinDetail = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();
 
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert('after', 'comments[-1]', [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: { _type: 'postedBy', _ref: user._id },
+          },
+        ])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment('');
+          setAddingComment(false);
+        });
+    }
+  };
+
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
+
     if (query) {
       client.fetch(query).then((data) => {
         setPinDetail(data[0]);
@@ -92,23 +116,50 @@ const PinDetail = ({ user }) => {
         </Link>
         <h2 className="mt-5 text-2xl">Comments</h2>
         <div className="max-h-370 overflow-y-auto">
-          {pinDetail?.comments?.map((comment, i) => {
+          {pinDetail?.comments?.map((comment, i) => (
             <div
               className="flex gap-2 mt-5 items-center bg-white rounded-lg"
               key={i}
             >
               <img
-                src={comment.postedBy.image}
-                alt="user-profile"
+                src={comment.postedBy?.image}
                 className="w-10 h-10 rounded-full cursor-pointer"
+                alt="user-profile"
               />
               <div className="flex flex-col">
-                <p className="font-bold">{comment.postedBy.userName}</p>
+                <p className="font-bold">{comment.postedBy?.userName}</p>
                 <p>{comment.comment}</p>
               </div>
-            </div>;
-          })}
+            </div>
+          ))}
         </div>
+      </div>
+      <div className="flex flex-wrap mt-6 gap-3">
+        <Link to={`user-profile/${pinDetail.postedBy._id}`}>
+          <img
+            className="w-8 h-8 rounded-full cursor-pointer"
+            src={
+              pinDetail.postedBy.image
+                ? pinDetail.postedBy.image
+                : 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png'
+            }
+            alt={`${pinDetail.postedBy} profile`}
+          />
+        </Link>
+        <input
+          className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+          type="text"
+          placeholder="Add a comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        ></input>
+        <button
+          type="button"
+          className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+          onClick={addComment}
+        >
+          {addingComment ? 'Posting comment...' : 'Post'}
+        </button>
       </div>
     </div>
   );
