31ed746a834bfe22895e1d9fea563ff83d41bf06
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-04 16:21:10 +0000

    starting user profile page

--- ispy_frontend/src/components/UserProfile/UserProfile.jsx
+++ ispy_frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from '@react-oauth/google';
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from '../../utils/data';
+import { client } from '../../client';
+import { MasonryLayout, Spinner } from '../index';
 
 const UserProfile = () => {
   return <div>UserProfile</div>;
